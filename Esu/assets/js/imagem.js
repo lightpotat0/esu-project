37ee84adab7233c654ad10eb0f3e6d6bae4e0dd9
felipppe2005 <113396587+felipppe2005@users.noreply.github.com>
2022-11-03 09:12:14 -0300

    oi

--- Esu/assets/js/imagem.js
+++ Esu/assets/js/imagem.js
@@ -1,9 +1,20 @@
-var imgAtual = "6.jpeg"
+var imgAtual = "2.jpeg"
 var imgAnterior = "01.png"
 
 
 function trocar(){
     document.getElementById("img").src = imgAtual;
+    let aux = imgAtual;
+    imgAtual = imgAnterior;
+    imgAnterior = aux
+}
+
+
+var imgAtual = "6.jpeg"
+var imgAnterior = "02.png"
+
+function trocar(){
+    document.getElementById("img1").src = imgAtual;
     let aux = imgAtual;
     imgAtual = imgAnterior;
     imgAnterior = aux
