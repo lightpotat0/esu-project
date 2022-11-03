4fda63a5027bfa70193f73abc1f4bae6091e05a7
felipppe2005 <113396587+felipppe2005@users.noreply.github.com>
2022-11-03 10:09:29 -0300

    oi

--- Esu/assets/js/imagem.js
+++ Esu/assets/js/imagem.js
@@ -1,21 +1,31 @@
-var imgAtual = "2.jpeg"
-var imgAnterior = "01.png"
+var img1 = document.getElementById('img')
+var img2 = document.getElementById('img1')
+var img3 = document.getElementById('img2')
 
 
-function trocar(){
-    document.getElementById("img").src = imgAtual;
-    let aux = imgAtual;
-    imgAtual = imgAnterior;
-    imgAnterior = aux
+function trocar0(){
+    img.setAttribute('src', '01.png')
+    img1.setAttribute('src', '02.png')
+    img2.setAttribute('src', '03.png')
 }
 
 
-var imgAtual = "6.jpeg"
-var imgAnterior = "02.png"
-
 function trocar(){
-    document.getElementById("img1").src = imgAtual;
-    let aux = imgAtual;
-    imgAtual = imgAnterior;
-    imgAnterior = aux
+     img.setAttribute('src', '3.jpeg')
+     img1.setAttribute('src', '6.jpeg')
+     img2.setAttribute('src', '5.jpeg')
+}
+
+
+function trocar1(){
+    img.setAttribute('src', 'estudio.jpeg')
+    img1.setAttribute('src', 'design.jpeg')
+    img2.setAttribute('src', '6.jpeg')
+}
+
+
+function trocar2(){
+    img.setAttribute('src', '1.jpeg')
+    img1.setAttribute('src', '2.jpeg')
+    img2.setAttribute('src', '4.jpeg')
 }
