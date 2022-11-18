6c3d7037acb74b81cc70b2fddd346fbab91cf66c
felipppe2005 <113396587+felipppe2005@users.noreply.github.com>
2022-11-18 10:02:05 -0300

    oi

--- Esu/assets/js/imagem.js
+++ Esu/assets/js/imagem.js
@@ -1,21 +1,21 @@
-var img1 = document.getElementById('img')
-var img2 = document.getElementById('img1')
-var img3 = document.getElementById('img2')
+var img = document.getElementById('img')
+var img1 = document.getElementById('img1')
+var img2 = document.getElementById('img2')
 
 
-function trocar0(){
+
+
+/*function trocar0(){
     img.setAttribute('src', '01.png')
     img1.setAttribute('src', '02.png')
     img2.setAttribute('src', '03.png')
 }
 
-
 function trocar(){
-     img.setAttribute('src', '3.jpeg')
-     img1.setAttribute('src', '6.jpeg')
-     img2.setAttribute('src', '5.jpeg')
+    img.setAttribute('src', '3.jpeg')
+    img1.setAttribute('src', '6.jpeg')
+    img2.setAttribute('src', '5.jpeg')
 }
-
 
 function trocar1(){
     img.setAttribute('src', 'estudio.jpeg')
@@ -23,9 +23,32 @@ function trocar1(){
     img2.setAttribute('src', '6.jpeg')
 }
 
-
 function trocar2(){
     img.setAttribute('src', '1.jpeg')
     img1.setAttribute('src', '2.jpeg')
     img2.setAttribute('src', '4.jpeg')
+}*/
+
+function trocar(){
+    img.setAttribute('src', '3.jpeg')
+    img1.setAttribute('src', '6.jpeg')
+    img2.setAttribute('src', '5.jpeg')
+}
+
+function trocar0(){
+    img.setAttribute('src', '01.png')
+    img1.setAttribute('src', '02.png')
+    img2.setAttribute('src', '03.png')
+}
+
+function trocar1(){
+    img.setAttribute('src', '2.jpeg')
+    img1.setAttribute('src', '4.jpeg')
+    img2.setAttribute('src', '1.jpeg')
+}
+
+function trocar2(){
+    img.setAttribute('src', 'campanha.jpeg')
+    img1.setAttribute('src', 'design.jpeg')
+    img2.setAttribute('src', 'estudio.jpeg')
 }
